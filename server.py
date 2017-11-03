bf9d2d535161def2dce2e8521752fa21e4f960f7
Ivanov Sergey <djapy@yandex.ru>
2017-11-03 16:15:12 +0300

    Add the logic of working with text

--- server.py
+++ server.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from typograf_text import typograf_text
 app = Flask(__name__)
 
 @app.route('/')
 def form():
     return render_template('form.html')
+
+
+@app.route('/', methods=['POST'])
+def changed_text():
+    input_text = request.form['text']
+    output_text = typograf_text(input_text)
+    return render_template('form.html',
+                           input_text=input_text,
+                           output_text=output_text)
+
 
 if __name__ == "__main__":
     app.run()
